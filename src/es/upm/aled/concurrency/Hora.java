2ae71a854b533d4605c23f49fc92342494d7d51e
AlvaroLlamasGonzalez <alvaro.llamasgon@alumnos.upm.es>
2025-10-30 18:48:00 +0100

    clase 30/10/2025

---
+++ src/es/upm/aled/concurrency/Hora.java
@@ -0,0 +1,20 @@
+package es.upm.aled.concurrency;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Hora extends Thread {
+
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				sleep(1000);
+				System.out.println(LocalDateTime.now().toString());
+			}
+		} catch (InterruptedException e) {
+			return;
+		}
+	}
+
+}
