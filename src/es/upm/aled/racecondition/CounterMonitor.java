2c8e76a803b049e038e55fba00081221287ac43b
AlvaroLlamasGonzalez <alvaro.llamasgon@alumnos.upm.es>
2025-11-07 19:05:56 +0100

    hoy

---
+++ src/es/upm/aled/racecondition/CounterMonitor.java
@@ -0,0 +1,20 @@
+package es.upm.aled.racecondition;
+
+// This class is Thread-Safe
+public class CounterMonitor extends Counter {
+
+	@Override
+	public synchronized int getCount() {
+		return count;
+	}
+	
+	@Override
+	public synchronized void setCount(int count) {
+		this.count = count;
+	}
+	
+	@Override
+	public synchronized void increase() {
+		count++; // Tampoco es atómico
+	}
+}
